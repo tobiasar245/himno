09229b8aec2da1c09b1035c47413c13b72a84753
neyen <pepepeemail@sandwich.com>
2025-11-26 21:13:18 -0300

    Primera version del himno

---
+++ Himno/src/himno/Himno.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package himno;
+
+/**
+ *
+ * @author b6
+ */
+public class Himno {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Oid mortales");
+        System.out.println("el grito sagrado");
+        System.out.println("Oid el ruido de rotas cadenas");
+        System.out.println("ved el trono a la noble repulgue");
+        System.out.println("Ya a su trono dignísimo cerraron");
+        System.out.println("las provincias unidas de Europa");
+        System.out.println("y los libres del mundo responden");
+        System.out.println("al gran pueblo argentino salud.");
+        System.out.println("Sean eternos los laureles");System.out.println("que supimos conseguir");
+        System.out.println("sean eternos los laureles");
+        System.out.println("que supimos conseguir");
+        System.out.println("coronados de gloria");
+        System.out.println("o juremos con gloria morir");
+        System.out.println("o juremos con gloria morir");
+        System.out.println("o juremos con gloria morir");
+    }
+}
